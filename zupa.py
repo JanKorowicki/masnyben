436610d0a43f4155ffa4ad0bbd9dd04ea478d670
JanKorowicki <124902159+JanKorowicki@users.noreply.github.com>
2023-02-16 16:22:06 +0100

    Add files via upload

---
+++ zupa.py
@@ -0,0 +1,32 @@
+#print("elo") #pisze elo
+
+#input() #czeka na odpowiedz uzytkownika
+
+#x = "stary ale jary"
+#print(len(x)) #liczy znaki w stringu i je wypluwa
+#print(x[0]) #pisze znak z stringa na pozycji 0 (czyli s)
+#print(x[0:3]) #to samo co na gorze tylko ze do 3 pozycji(jak na początku sie nie da nic to znaczy ze 0)
+#print(x[-1]) #daje znak licząc pozycje od konca (pomija 0 czyli wartos min -1)
+#print(x[:]) #działą tak samo jak print(x)
+#print(x[::2]) #wypisuje co 2 znaki
+ #print(cos[x:y:z]
+ #x - poczatek
+ #y - koniec
+ #z - krok
+
+
+#imie = "dzosz"
+
+#nazwisko = "korowicki"
+#print(imie + " " + nazwisko)
+#print(imie, nazwisko)
+#print (imie * 3, nazwisko)
+#print(f"elo {imie} glupi sraczarzu")
+#print(type(imie)) # wypisuje nam rodzaj zmiennej imie
+
+#cos = input("podaj swoj wiek : ")
+#print(int(cos)+2)
+james_bond = 7
+print(str(james_bond).zfill(3))
+
+
