7a65be3e1271b0c44cdb7a93937503feacd19fde
JanKorowicki <124902159+JanKorowicki@users.noreply.github.com>
2023-02-23 20:36:48 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,54 +1,14 @@
+count = 0
 
-# course = "  Benol Fenol" #<-- string
-# print(course)
-# print(course.upper())#drukuje course ale na capsie
-# print(course.lower())#wszystko z malej drukuje
-# print(course.title())#robi z tego tytul
-# print(course.lstrip())#l wycina lewa strone a r prawa
-# print(course.find("Fe"))#wyszukuje pozycje podanego znaku
-# print(course.replace("e", "x"))#zamienia x na y
-# print("Be" in course)#sprawdza czy coś znajduje sie w stringu
-# print("Be" not in course)#sprawdza czy cos nie znajduje sie w stringu
+for zmienna in range (1, 101):
+    if (zmienna % 5 == 0) and (zmienna % 3 == 0):
+        print("FizzBuzz")
+    elif zmienna % 3 == 0:
+        print("Fizz")
+        count = count + 1
+    elif zmienna % 5 == 0:
+        print("Buzz")
+    else:
+        print(zmienna)
 
-
-# print(10 / 3)#dzieli 10 na 3
-# print(10 // 3)#to samo + obcina ulamek
-# print(10 % 3)#daje nam reszte dzielenia
-# print(10 ** 3)#x do potegi y
-
-
-# import math
-# print(round(2.9))#zaokrogla
-# print(abs(-2.9))#wartość bezwzględna
-# print(math.ceil(2.2))#zaokrągla do góry
-# print(math.floor(2.2))#zaokrogla w dol
-
-
-# x = input("podaj cos: ")
-# y = int(x) + 1
-# print(f"x: {x}, y: {y}")
-
-# print(ord("a"))#daje nam numer w ascii
-# print(ord("b"))
-
-# zmienna = input("podaj zmienna: ")
-# if int(zmienna) == 120:
-#     print("masnyben")
-# else:
-#     print("suiiii")
-
-
-# while 1:
-#     wiek = input("podaj wiek: ")
-#     if wiek == "koniec":
-#         break
-#     elif int(wiek) >= 18:
-#         print("wkraczamy do akcji")
-#     else:
-#         print("odwrot")
-
-#for zmienna in range(2,10,2):
-    #print(zmienna)
-
-
-
+print(f"wypisało fizz {count} razy")
