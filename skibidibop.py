312f1ab7ef59b788f06e369abdefcb6dc26cc16a
JanKorowicki <124902159+JanKorowicki@users.noreply.github.com>
2023-03-16 20:58:09 +0100

    Add files via upload

---
+++ skibidibop.py
@@ -0,0 +1,19 @@
+#while True:
+    #liczba = input("podaj liczbe którą chcesz zmierzyć(jezeli chcesz skonczyc wpisz end): ")
+   # if liczba == "end":
+        #break
+    #liczba = str(liczba)
+    #print(len(liczba))
+
+#while True:
+ #   cchk = input("chcesz kontynuować? (t/n): ")
+  #  if cchk == "n":
+   #     break
+    #else:
+     #   kosztprodukcji = input("podaj cene wyprodukowania produktu/sztk: ")
+      #  cenasprzedazy = input("podaj cene sprzedazy produktu/sztk: ")
+       # ilosc = input("podaj ilość wyprodukowanego produktu: ")
+        #sprzedaz = input("podaj ilosc sprzedanych produktow: ")
+
+ #       profit = (int(cenasprzedazy) * int(sprzedaz)) - (int(kosztprodukcji) * int(ilosc))
+  #      print(profit)
